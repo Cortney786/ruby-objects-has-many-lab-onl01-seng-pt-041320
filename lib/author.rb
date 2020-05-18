e949b922eaad341b6757879489717b15f840bb94
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-05-18 20:14:14 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author 
   
   attr_accessor :name, :posts 
-  
+  @@post_count
 
   def initialize(name) 
     @name = name 
@@ -17,11 +17,13 @@ class Author
 def add_post(post) 
   @posts << post 
   post.author = self 
+  @@post_cont += 1
 end 
 
 def add_post_by_title(post_title) 
   post = Post.new(post_title) 
-  @posts < post 
+  @posts << post 
+  @@post_count += 1
 end 
 
 def self.post_count 
